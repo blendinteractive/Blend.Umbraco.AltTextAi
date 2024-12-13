467ba145cf4e40224b0cc255483343ec555471e7
paulsterling <paul@motusconnect.com>
2024-12-13 13:03:10 -0800

    Add logging and refactor alt text generation with additional AltText.ai options.
    - Update MediaSavedAltTextHandler to use ILogger for logging
    - Refactor Handle method to log alt text status and queue background work
    - Update error handling and logging in RequestAltTextGeneration method
    - Change ResizeImage method to return byte array instead of MemoryStream
    - Initialize properties with defaults in AltTextAi and add new configuration properties: AltTextLengthToSkip and AltTextKeyWords

--- Blend.Umbraco.AltTextAi/Configuration/AltTextAi.cs
+++ Blend.Umbraco.AltTextAi/Configuration/AltTextAi.cs
@@ -5,9 +5,20 @@ namespace Blend.Umbraco.AltTextAi.Configuration;
 [DataContract]
 public class AltTextAi
 {
-    [DataMember(Name="AltTextAiApiKey")]
-    public string AltTextAiApiKey { get; set; }
-    
-    [DataMember(Name="ImageAltTextProperty")]
-    public string ImageAltTextProperty { get; set; }
+    [DataMember(Name = "AltTextAiApiKey")]
+    public string AltTextAiApiKey { get; set; } = string.Empty;
+
+    [DataMember(Name = "ImageAltTextProperty")]
+    public string ImageAltTextProperty { get; set; } = string.Empty;
+
+    // the length of the alt text to skip the AI generation
+    // shorter than this setting will (re)generate the alt text
+    // set to 0 to skip generation if alt text is not empty
+    [DataMember(Name = "AltTextLengthToSkip")]
+    public int AltTextLengthToSkip { get; set; } = 0;
+
+    // an array of up to 6 keywords to use in the AI generation
+    // useful for adding SEO context to the generated alt text
+    [DataMember(Name = "AltTextKeyWords")]
+    public string[] AltTextKeyWords { get; set; } = Array.Empty<string>();
 }
